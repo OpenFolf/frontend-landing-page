4387d2f7b491d3637ec923b64e134097518459b4
Aegir Aexx <aegir.tomasson@gmail.com>
2020-04-21 11:51:58 +0000

    Initial setup and install

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 100,
+  trailingComma: "all",
+  arrowParens: "always",
+  vueIndentScriptAndStyle: true,
+  endOfLine: "lf",
+  proseWrap: "never",
+};
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-btn>button placeholder</v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <h1>Placeholder openFOLF</h1>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
+  export default {
+    name: "App",
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
