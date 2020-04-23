6db4daa1cb7dc4c34b56f3c37c9ce43e30224c72
Aegir Aexx <aegir.tomasson@gmail.com>
2020-04-23 22:15:51 +0000

    Ready

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.export = {
-  chainWebpack: (config) => {
-    config.plugin("html").tap((args) => {
-      args[0].title = "openFOLF | Real-Time Disc Golf Tracking";
-      return args;
-    });
-  },
-};
