5429f5de177512734ff8ab84a86c7d333c2cf434
Aegir Aexx <aegir.tomasson@gmail.com>
2020-04-23 19:27:07 +0000

    Pre-refactor

--- src/components/HeroFooter.vue
+++ src/components/HeroFooter.vue
@@ -1,8 +1,17 @@
 <template>
-  <footer class="h-64 flex flex-row justify-center items-center">
-    <div>LOGO</div>
-    <div>POJECT MAP</div>
-    <div>CONTACT</div>
+  <footer class="h-36 ma-0 flex flex-row justify-center items-center">
+    <div class="w-1/6 mt-6 ml-6 flex flex-row items-start">
+      <img class="of--logo-small mr-4" src="/images/openfolfwhite.svg" alt="" />
+      <span class="text-white text-3xl">openFOLF</span>
+    </div>
+    <div class="w-5/6">
+      <ul class="flex flex-row justify-around">
+        <li class="text-white text-2xl">Home</li>
+        <li class="text-white text-2xl">Application</li>
+        <li class="text-white text-2xl">Source Code Repository</li>
+        <li class="text-white text-2xl">Documentation</li>
+      </ul>
+    </div>
   </footer>
 </template>
 
@@ -20,5 +29,8 @@
 <style scoped>
   footer {
     background-color: #005737;
+  }
+  .of--logo-small {
+    height: 3rem;
   }
 </style>
