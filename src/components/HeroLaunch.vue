9bcdbb838fb62ccbeedbbab5a68a73ef20449166
Aegir Aexx <aegir.tomasson@gmail.com>
2020-04-23 13:27:15 +0000

    Layout and content ready.

--- src/components/HeroLaunch.vue
+++ src/components/HeroLaunch.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="openfolf--hero-1 h-screen flex flex-row">
-    <div class="openfolf--hero-left pl-20 w-6/12 flex flex-col justify-center items-center">
-      <img class="openfolf--logo-hero" src="/images/openfolfgreen.svg" alt="openfolf logo" />
+  <div class="of--hero h-screen flex flex-row">
+    <div class="of--hero-left pl-20 w-6/12 flex flex-col justify-center items-center">
+      <img class="of--logo-hero" src="/images/openfolfgreen.svg" alt="openfolf logo" />
       <span class="text-6xl font-bold">openFOLF</span>
     </div>
-    <div class="openfolf--hero-right pr-20 w-6/12 flex flex-col justify-center items-center">
-      <h1 class="openfolf--text-green text-center font-bold text-5xl mb-6">
+    <div class="of--hero-right pr-20 w-6/12 flex flex-col justify-center items-center">
+      <h1 class="of--text-green text-center font-bold text-5xl mb-6">
         Real-time<br />
         Disc Golf Tracking
       </h1>
       <p class="text-xl max-w-xl mb-10 text-center">
-        With <span class="openfolf--text-green font-bold text-2xl">openFOLF</span> our users can
-        keep track their matches in real time. All users that are together in a single match recieve
-        real time updates to any changes on the comunal scorecard. <br />
+        With <span class="of--text-green font-bold text-2xl">openFOLF</span> you can easily keep
+        track of your game as you play it with a real-time comunal scorecard. The comunal scorecard
+        is shared between all players in your party, giving everyone a chance to log their own score
+        and keep track of the other players with real-time updates. <br /><br />
         <em>The application is designed for mobile devices.</em>
       </p>
       <p class="text-xl max-w-xl mb-10 font-bold text-center">
@@ -39,13 +40,13 @@
 </script>
 
 <style scoped>
-  div.openfolf--hero-1 {
-    background-image: url("/images/hero6.png");
+  div.of--hero {
+    background-image: url("/images/hero-launch.png");
   }
-  img.openfolf--logo-hero {
+  img.of--logo-hero {
     max-height: 60vh;
   }
-  .openfolf--text-green {
+  .of--text-green {
     color: #005737;
   }
   .button {
